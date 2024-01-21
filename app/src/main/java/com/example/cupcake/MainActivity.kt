2d4e1bcf513b3915ed2602f25c4b3bda759a0884
asteriinn <nishiolatte@gmail.com>
2024-01-21 16:02:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.cupcake
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
+import com.example.cupcake.ui.ui.CupcakeApp
+import com.example.cupcake.ui.ui.CupcakeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContent {
+            CupcakeTheme {
+                CupcakeApp()
+            }
+        }
+    }
+}
